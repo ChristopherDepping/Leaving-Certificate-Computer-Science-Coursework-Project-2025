ff700802468d28f357c2c9c46a10c36c4818b065
ChristopherDepping <chrisdepping1@gmail.com>
2025-03-21 09:48:22 +0000

    Delete start_app.bat

--- start_app.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo Starting Flask/Dash app with Waitress...
-cd /d "%~dp0"
-start "" cmd /k "py -m waitress --listen=127.0.0.1:5000 app:server"
-timeout /t 5 >nul
-start "" http://127.0.0.1:5000/
-exit 	
